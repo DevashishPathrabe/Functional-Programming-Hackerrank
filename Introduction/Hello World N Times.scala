75ca2e5eb81d40bcbf98572ac217bd3152c4ece6
Varun Upadhyay <varun.u28@gmail.com>
2017-10-06 16:28:52 -0700

    Added 2 solutions

---
+++ Introduction/Hello World N Times.scala
@@ -0,0 +1,19 @@
+object Solution extends App {
+
+object Solution {
+
+    def main(args: Array[String]) {
+        val sc = new java.util.Scanner (System.in);
+        var n = sc.nextInt();
+    }
+}
+
+def f(n: Int) = {
+    for (i <- 1 to n) {
+        println("Hello World")
+    }
+}
+
+  var n = scala.io.StdIn.readInt
+  f(n)
+}
